787e8697c5cbc16cc644e29fb014f6d28bd53663
Julien Langer <julien.langer@gmail.com>
2020-02-28 19:13:45 +0100

    PWM Based Buzzer

--- modules/buzzer/__init__.py
+++ modules/buzzer/__init__.py
@@ -1,30 +1,36 @@
 import time
 from thread import start_new_thread
 from modules import cbpi
+import os
 
-try:
-    import RPi.GPIO as GPIO
-    import wiringpi
-except Exception as e:
-    pass
+PWM_PATH = '/sys/class/pwm/'
 
 class Buzzer(object):
 
     sound = ["H", 0.1, "L", 0.1, "H", 0.1, "L", 0.1, "H", 0.1, "L"]
-    def __init__(self, gpio, beep_level):
+    def __init__(self, pwm, pwmchip, beep_level):
         try:
-            cbpi.app.logger.info("INIT BUZZER NOW GPIO%s" % gpio)
+            cbpi.app.logger.info("INIT BUZZER PWMCHIP %s PWM %s" % (pwmchip, pwm))
             #self.gpio = int(gpio)
-            self.gpio=27
+            self.pwmchip=int(pwmchip)
+            self.pwm=int(pwm)
             self.beep_level = beep_level
-            wiringpi.wiringPiSetup()
-            wiringpi.softToneCreate(self.gpio)
-            #GPIO.setmode(GPIO.BCM)
-            #GPIO.setup(self.gpio, GPIO.OUT)
+
+            if not os.path.exists(PWM_PATH + ('pwmchip%d/pwm%d' % (self.pwmchip, self.pwm))):
+                with open(PWM_PATH + ('pwmchip%d/export' % self.pwmchip), 'w') as fp:
+                    fp.write(str(pwm))
+
+	    with open(PWM_PATH + ('pwmchip%d/pwm%d/period' % (self.pwmchip, self.pwm)), 'w') as fp:
+                fp.write(str(1000000))
+
+	    with open(PWM_PATH + ('pwmchip%d/pwm%d/duty_cycle' % (self.pwmchip, self.pwm)), 'w') as fp:
+                fp.write(str(300000))
+
+ 
             self.state = True
             cbpi.app.logger.info("BUZZER SETUP OK")
         except Exception as e:
-            cbpi.app.logger.info("BUZZER EXCEPTION %s" % str(e))
+            cbpi.app.logger.error("BUZZER EXCEPTION %s" % str(e))
             self.state = False
 
     def beep(self):
@@ -38,16 +44,20 @@ class Buzzer(object):
                     if (isinstance(i, str)):
                         if i == "H" and self.beep_level == "HIGH":
                            # GPIO.output(int(self.gpio), GPIO.HIGH)
-                            wiringpi.softToneWrite(self.gpio, 1000);
+	                    with open(PWM_PATH + ('pwmchip%d/pwm%d/enable' % (self.pwmchip, self.pwm)), 'w') as fp:
+                                fp.write('1')
                         elif i == "H" and self.beep_level != "HIGH":
                            # GPIO.output(int(self.gpio), GPIO.LOW)
-                            wiringpi.softToneWrite(self.gpio, 0);
+	                    with open(PWM_PATH + ('pwmchip%d/pwm%d/enable' % (self.pwmchip, self.pwm)), 'w') as fp:
+                                fp.write('0')
                         elif i == "L" and self.beep_level == "HIGH":
                            # GPIO.output(int(self.gpio), GPIO.LOW)
-                            wiringpi.softToneWrite(self.gpio, 0);
+	                    with open(PWM_PATH + ('pwmchip%d/pwm%d/enable' % (self.pwmchip, self.pwm)), 'w') as fp:
+                                fp.write('0')
                         else:
                             #GPIO.output(int(self.gpio), GPIO.HIGH)
-                            wiringpi.softToneWrite(self.gpio, 1000);
+	                    with open(PWM_PATH + ('pwmchip%d/pwm%d/enable' % (self.pwmchip, self.pwm)), 'w') as fp:
+                                fp.write('1')
                     else:
                         time.sleep(i)
             except Exception as e:
@@ -57,9 +67,11 @@ class Buzzer(object):
 
 @cbpi.initalizer(order=1)
 def init(cbpi):
-    gpio = cbpi.get_config_parameter("buzzer", 22)
+    cbpi.app.logger.info("INIT buzz")
+    pwmchip = cbpi.get_config_parameter("buzzer_pwmchip", 0)
+    pwm = cbpi.get_config_parameter("buzzer_pwm", 0)
     beep_level = cbpi.get_config_parameter("buzzer_beep_level", "HIGH")
 
-    cbpi.buzzer = Buzzer(gpio, beep_level)
+    cbpi.buzzer = Buzzer(pwm, pwmchip, beep_level)
     cbpi.beep()
     cbpi.app.logger.info("INIT OK")
